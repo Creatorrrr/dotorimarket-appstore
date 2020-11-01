ed78a82eead9cfd2e0cb8a13ea07cb439d951635
csi <ckthdlr@naver.com>
2020-11-02 00:04:12 +0900

    file upload

--- backend/routes/app-router.js
+++ backend/routes/app-router.js
@@ -1,36 +1,35 @@
 'use strict';
 
+const fs = require('fs');
 const createError = require('http-errors');
 const express = require('express');
 const router = express.Router();
 const HttpConfig = require('../configs/http-config');
 const FileConfig = require('../configs/file-config');
 
+const { uploadCfg } = require("../configs/upload-config");
+
 router.get('/v1/apps/:fileName', function(req, res, next) { // ************** querystring으로 jwt 인증
   const fileName = req.params.fileName;
   res.download(`${FileConfig.FILE_PATH}/${fileName}`);
 });
 
-router.post('/v1/apps/', function(req, res, next) { // ************** querystring으로 jwt 인증
-  const ios = req.files.ios;
-  const android = req.files.android;
+router.post('/v1/apps/', uploadCfg().fields([{ name: "ios" }, { name: "android" }]), function(req, res, next) { // ************** querystring으로 jwt 인증
+  const ios = req.files.ios[0];
+  const android = req.files.android[0];
 
   if (ios) {
-    ios.mv(
-      `${FileConfig.FILE_PATH}/dotori-ios.ipa`,
-      (err) => {
-        if (err) throw createError(HttpConfig.INTERNAL_SERVER_ERROR.statusCode, `${HttpConfig.INTERNAL_SERVER_ERROR.message} (에러정보: ${err})`);
-      }
-    );
+    fs.rename(ios.path, `${FileConfig.FILE_PATH}/dotori-ios.ipa`, 
+    (err) => {
+      if (err) throw createError(HttpConfig.INTERNAL_SERVER_ERROR.statusCode, `${HttpConfig.INTERNAL_SERVER_ERROR.message} (에러정보: ${err})`);
+    });
   }
 
   if (android) {
-    android.mv(
-      `${FileConfig.FILE_PATH}/dotori-android.apk`,
-      (err) => {
-        if (err) throw createError(HttpConfig.INTERNAL_SERVER_ERROR.statusCode, `${HttpConfig.INTERNAL_SERVER_ERROR.message} (에러정보: ${err})`);
-      }
-    );
+    fs.rename(android.path, `${FileConfig.FILE_PATH}/dotori-android.apk`, 
+    (err) => {
+      if (err) throw createError(HttpConfig.INTERNAL_SERVER_ERROR.statusCode, `${HttpConfig.INTERNAL_SERVER_ERROR.message} (에러정보: ${err})`);
+    });
   }
 
   res.json({
